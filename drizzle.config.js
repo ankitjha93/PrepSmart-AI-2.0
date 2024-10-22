e65041b7aa0076b22e5304f479fc9357c65b4c11
ankitjha93 <93ankitjha@gmail.com>
2024-10-22 17:07:52 +0530

    finally done

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: 'postgresql://AI%20Mocker_owner:npDrAV5Heg0s@ep-odd-dust-a53bfxgw-pooler.us-east-2.aws.neon.tech/AI%20Mocker?sslmode=require',
+    }
+  };
